d25ffe1101b0d23a126cdf17c8ccb89c6cdeaf8a
salmanpatel <salmanpatel131@gmail.com>
2015-03-21 21:30:45 +0530

    Event.py :: First commit without any method

---
+++ Event.py
@@ -0,0 +1,7 @@
+class Event:
+	"""docstring for Event"""
+	# arrival=0, departure=1, quantumExpired=2, scheduleNextRequest=3, requestTimeout=4
+	def __init__(self, eventType, id):
+		self.timestamp = timestamp
+		self.eventType = 0
+		self.id = id
