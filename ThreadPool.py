2d1fd4f1e06ba774e24754d09a0059ef728840a9
salmanpatel <salmanpatel131@gmail.com>
2015-03-21 17:42:02 +0530

    ThreadPool class :: freeThread() to be written

---
+++ ThreadPool.py
@@ -0,0 +1,21 @@
+class  ThreadPool:
+	"""ThreadPool for  """
+	# Index of thread will be from 1 to max number of threads
+	def __init__(self, numberOfThreads):
+		self.numberOfThreads = numberOfThreads
+		numberOfBusyThreads = 0
+		# Threadstatus is a dictionary where key will be thread id and value will be a list whose first element will be request id and 2nd elelemt will be core id
+		threadStatus = {}
+	def allocateThread(self, requestId, coreId, threadId):
+		self.numberOfBusyThreads = self.numberOfBusyThreads + 1
+		threadStatus[threadId] = [requestId, coreId]
+	def freeThread(self, requestId):
+		# yet to write logic for extracting key of thread to be deleted
+		del self.threadStatus[]
+	def getFreeThreadId(self):
+		# Returns a threadId of a free thread otherwise -1
+		if numberOfBusyThreads < numberOfThreads:
+			# find thread id with min index
+			return min(self.threadStatus)
+		else:
+			return -1
